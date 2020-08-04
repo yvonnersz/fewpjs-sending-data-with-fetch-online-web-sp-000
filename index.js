905d2a0289baebd9bfc127e9a5fe52573c51cd3f
yvonnersz <yvonnersz@gmail.com>
2020-08-03 19:02:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+    
+    const formData = {
+        name: name,
+        email: email,
+    };
+
+    const constObj = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept": "application/json"
+        },
+
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', constObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.textContent = object.id;
+        })
+        .catch(function(error) {
+            document.body.textContent = error.message;
+        })
+}
