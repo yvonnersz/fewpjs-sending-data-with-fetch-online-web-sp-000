4b2987f54f7dbb695b56b39d1efeaff5a8938349
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-04 11:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Add your code here
 
 function submitData(name, email) {
@@ -28,3 +29,35 @@ function submitData(name, email) {
             document.body.textContent = error.message;
         })
 }
+=======
+// Add your code here
+
+function submitData(name, email) {
+
+    const formData = {
+        name: name,
+        email: email,
+    };
+
+    const constObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', constObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.textContent = object.id;
+        })
+        .catch(function(error) {
+            document.body.textContent = error.message;
+        })
+}
+>>>>>>> 3064fc0677a0b6ed2fd9c78adf870784c688270a
